cef14e1bc0a6d66782cb5c33cb448de25506ebb2
MagdalenaStranska <stranska.magdalena@gmail.com>
2024-11-11 23:09:55 +0100

    Pridavam reseni

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+// Cvičení: Funkce jako argumenty
+// Hesla
+// Vytvořte si repozitář ze šablony cviceni-hesla se stránkou, která obsahuje tři různé funkce na generování hesel. Každá funkce vygeneruje heslo zadané délky s určitou bezpečnostní silou. Kód funkcí zkoumat nemusíte, obsahuje věci, které jsme zatím neprobírali.
+
+// Příklad samostatného použití jednotlivých funkcí:
+
+// weakPassword(5); // → '01234'
+// mediumPassword(8); // → '48140525'
+// strongPassword(6); // → 'azc7mw'
+// Napište funkci createAccount, která se bude tvářit, že zakládá nový uživatelský účet. Funkce bude mít dva parametry user a generatePassword. První bude uživatelské jméno a druhý bude funkce, pomocí které se má vygenerovat heslo pro tento účet. Funkce createAccount vrátí řetězec, který bude obsahovat jméno uživatele a heslo vygenerované voláním funkce generatePassword. Funkci generatePassword při volání předejte číslo 9 jako délku hesla.
+
+// Na konci javascriptového kódu vyzkoušejte založit více různých účtů s různými typy hesel. Například:
+
+// document.body.innerHTML += `
+// 	<p>${createAccount('Míša', weakPassword)}</p>
+// 	<p>${createAccount('Řízek', mediumPassword)}</p>
+// 	<p>${createAccount('Mápodčepicí', strongPassword)}</p>
+// `;
+// by mělo vepsat do stránky něco jako:
+
+// Uživatel Míša s heslem 012345678
+// Uživatel Řízek s heslem 074031827
+// Uživatel Mápodčepicí s heslem mwwf9epts
+
 const weakPassword = (len) => {
   let result = '';
   for (let i = 0; i < len; i++) {
@@ -27,3 +51,21 @@ const strongPassword = (len) => {
 
   return result;
 };
+
+const createAccount = (user, generatePassword) => { 
+  const password = generatePassword(9);
+  return `Uživatel ${user} s heslem ${password}.`;
+};
+document.body.innerHTML += `
+	<p>${createAccount('Míša', weakPassword)}</p>
+	<p>${createAccount('Řízek', mediumPassword)}</p>
+	<p>${createAccount('Mápodčepicí', strongPassword)}</p>
+`;
+
+// Reseni a poznamky od Oleksie:
+// const createAccount = (user, generatePassword) => {  // Napište funkci createAccount, která se bude tvářit, že zakládá nový uživatelský účet. Funkce bude mít dva parametry user a generatePassword. První bude uživatelské jméno a druhý bude funkce,
+//   const password = generatePassword(9); // 9- znamená počet znaků v našem hesle
+//   return `Uživatel ${user} s heslem ${password}.`;
+// };
+// document.body.innerHTML += `
+//      <p>${createAccount('Míša', weakPassword)}</p>`; //Zde při volání funkce createAccount první parametr bude user a druhý generatePassword - který nabývá hodnoty weakPassword, mediumPassword nebo strongPassword z počet znaků 9
